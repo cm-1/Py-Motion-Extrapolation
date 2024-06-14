f23e95764f5eb2683311c701ee69419c309d03a3
Christopher Mossman <christopher.mossman@ucalgary.ca>
2024-06-14 12:56:03 -0600

    Fix to how Python plotter handers frame count steps > 0.

--- gtPlotter.py
+++ gtPlotter.py
@@ -143,7 +143,8 @@ sequence_names = [
 posePathGT = datasetDir / sequence_names[11] / body_names[1] / "pose.txt"
 print("Pose path:", posePathGT)
 
-posePathCalc = poseExportDir / ("cvOnlySkip0_poses_" + sequence_names[11] + "_" + body_names[1] +".txt")
+skipAmt = 2
+posePathCalc = poseExportDir / ("cvOnlySkip" + str(skipAmt) + "_poses_" + sequence_names[11] + "_" + body_names[1] +".txt")
 
 patternNum = r"(-?\d+\.?\d*)"
 patternTrans = re.compile((r"\s+" + patternNum) * 3 + r"\s*$")
@@ -187,9 +188,11 @@ def plotGTvsCalc(gt, calc, showOnlyGT = False):
     z = gt[:, 2]
     w = np.arange(len(x))#translationsNP.shape[0])
 
+    lastFrameNum = len(calc) * (skipAmt + 1)
+
     init_k = 10
     init_c = 10
-    init_t = len(calc)
+    init_t = lastFrameNum
     init_endpt = -1
 
     curvePts = cinpact.getSplinePts(calc, init_c, init_k, 1000)
@@ -202,8 +205,9 @@ def plotGTvsCalc(gt, calc, showOnlyGT = False):
     ax = fig.add_subplot(projection='3d')
     scatterGT = ax.scatter(x, y, z, marker='.', edgecolors=colors)
     if not showOnlyGT:
-        scatterCalc = ax.scatter(calc[:, 0], calc[:, 1], calc[:, 2], marker='+', edgecolors=colors)
-        scatterCalc.set_edgecolor(colors[:len(calc)]) # Need to do this again to make it look right
+        colorsCalc = colors[1::(skipAmt + 1)]
+        scatterCalc = ax.scatter(calc[:, 0], calc[:, 1], calc[:, 2], marker='+', edgecolors=colorsCalc)
+        scatterCalc.set_edgecolor(colorsCalc) # Need to do this again to make it look right
 
         line = ax.plot(curvePts[:, 0], curvePts[:, 1], curvePts[:, 2])
 
@@ -229,25 +233,25 @@ def plotGTvsCalc(gt, calc, showOnlyGT = False):
             ax = tAx,
             label ='t',
             valmin = 0,
-            valmax = len(calc),
+            valmax = lastFrameNum,
             valstep = 1,
             valinit=init_t
         )
 
         def updateDisplay(_):
-            end = int(tSlider.val)
-            endGT = end + 1
+            end = int(tSlider.val) // (skipAmt + 1)
+            endGT = int(tSlider.val) + 1
             scatterGT.set_offsets(gt[:endGT, :2])
             scatterGT.set_3d_properties(gt[:endGT, 2], 'z')
             scatterGT.set_edgecolor(colors[:endGT])
             scatterCalc.set_offsets(calc[:end, :2])
             scatterCalc.set_3d_properties(calc[:end, 2], 'z')
-            scatterCalc.set_edgecolor(colors[:end])
+            scatterCalc.set_edgecolor(colors[1:(end * skipAmt)+1:(skipAmt + 1)])
             fig.canvas.draw_idle()
 
         def updatePts(_):
             global curvePts
-            curvePts = cinpact.getSplinePts(calc[:init_endpt:3], cSlider.val, kSlider.val, 1000)
+            curvePts = cinpact.getSplinePts(calc[:init_endpt], cSlider.val, kSlider.val, 1000)
             end = int(tSlider.val)
             line[0].set_data_3d(
                 curvePts[:end, 0], curvePts[:end, 1], curvePts[:end, 2]
@@ -259,7 +263,7 @@ def plotGTvsCalc(gt, calc, showOnlyGT = False):
         kSlider.on_changed(updatePts)
         cSlider.on_changed(updatePts)
         tSlider.on_changed(updateDisplay)
-
+        
     plt.show()
 
 plotGTvsCalc(rotationsGTNP, rotationsCalcNP, False)
