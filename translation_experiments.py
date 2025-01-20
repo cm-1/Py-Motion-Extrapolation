e435dbe0ef9ac8a0a3344b9db1c62c196368429f
Christopher Mossman <christopher.mossman@ucalgary.ca>
2025-01-20 11:31:00 -0700

    Confidence interval result + full mat3 vel frames.

--- translation_experiments.py
+++ translation_experiments.py
@@ -38,7 +38,7 @@ for skip_amt in range(3):
         calculator = BCOT_Data_Calculator(combo[0], combo[1], skip_amt)
         rotations = calculator.getRotationMatsGTNP(True)
         ck = (skip_amt, ) + combo[:2]
-        all_rotations_T[ck] = np.moveaxis(rotations, -2, -1) # transposes
+        all_rotations_T[ck] = np.swapaxes(rotations, -1, -2) # transposes
         all_translations[ck] = calculator.getTranslationsGTNP(True)
 
 #%%
@@ -57,6 +57,7 @@ local_err_lists = get_start_data_struct()
 vel_deg1_err_lists = get_start_data_struct()
 vel_deg2_err_lists = get_start_data_struct()
 
+default_acc_dir = np.array([1.0, 0.0, 0.0])
 for skip_amt in range(3):
     for combo in combos:
         ck = (skip_amt, ) + combo[:2]
@@ -72,15 +73,34 @@ for skip_amt in range(3):
         
         deg2_vels = deg1_vels[1:] + half_deg2_acc
 
+        deg2_acc_lpad = np.empty_like(deg1_vels)
+        deg2_acc_lpad[1:] = deg2_acc
+        deg2_acc_lpad[0] = default_acc_dir
+
+        deg1_vel_frames = pm.getOrthonormalFrames(deg1_vels, deg2_acc_lpad)
+        deg2_vel_frames = pm.getOrthonormalFrames(deg2_vels, deg2_acc)
+
+        # Transposes
+        deg1_vel_frames = np.swapaxes(deg1_vel_frames, -1, -2)
+        deg2_vel_frames = np.swapaxes(deg2_vel_frames, -1, -2)
+
+        # def colList(mats):
+        #     return (mats[..., 0], mats[..., 1], mats[..., 2])
+
+        # if not pm.areAxisArraysOrthonormal(colList(deg1_vel_frames), loud=True):
+        #     raise Exception("deg1 orthonormal issue!")
+        # if not pm.areAxisArraysOrthonormal(colList(deg2_vel_frames), loud=True):
+        #     raise Exception("deg2 orthonormal issue!")
+
         temp_preds = dict()
         temp_preds["static"] = prev_translations
         temp_preds["vel-deg1"] = translations[1:-1] + deg1_vels
         temp_preds["vel-deg2"] = translations[2:-1] + deg2_vels
         temp_preds["acc-deg2"] = temp_preds["vel-deg2"] + half_deg2_acc
 
-        unit_deg1_vels = pm.normalizeAll(deg1_vels)
-        next_unit_deg1_vels = unit_deg1_vels[1:]
-        unit_deg2_vels = pm.normalizeAll(deg2_vels)
+        # unit_deg1_vels = pm.normalizeAll(deg1_vels)
+        next_deg1_vel_frames = deg1_vel_frames[1:]
+        # unit_deg2_vels = pm.normalizeAll(deg2_vels)
 
         for pk, preds in temp_preds.items():
             len_diff = n_next_translations - len(preds)
@@ -92,17 +112,17 @@ for skip_amt in range(3):
                 rt_mats[len_diff:-1], errs
             ))
 
-            fitted_unit_deg1_vels = unit_deg1_vels
+            deg1_vel_frame_subset = deg1_vel_frames
             if len_diff == 2:
-                fitted_unit_deg1_vels = next_unit_deg1_vels
+                deg1_vel_frame_subset = next_deg1_vel_frames
 
             deg1_len_diff = max(1 - len_diff, 0)
-            vel_deg1_err_lists[skip_amt][pk][combo[-1]].append(pm.einsumDot(
-                errs[deg1_len_diff:], fitted_unit_deg1_vels
+            vel_deg1_err_lists[skip_amt][pk][combo[-1]].append(
+                pm.einsumMatVecMul(deg1_vel_frame_subset, errs[deg1_len_diff:]
             ))
 
-            vel_deg2_err_lists[skip_amt][pk][combo[-1]].append(pm.einsumDot(
-                errs[2 - len_diff:], unit_deg2_vels
+            vel_deg2_err_lists[skip_amt][pk][combo[-1]].append(
+                pm.einsumMatVecMul(deg2_vel_frames, errs[2 - len_diff:]
             ))
 
 def concatErrs(err_list_data_struct):
@@ -159,6 +179,9 @@ vel_deg2_stats_struct = getStatsStruct(vel_deg2_errs)
 
 #%%
 
+print("Note: Default acc dir used for first deg1-vel frame is:", list(default_acc_dir))
+print() # newline
+
 for skip_amt in range(3):
     print("\nSkip {}:".format(skip_amt))
 
@@ -184,19 +207,22 @@ for skip_amt in range(3):
             for name, stat in zip(ref_frame_names, all_stats):
                 if "v" in name:
                     if "static" in pk:
-                        curr_mclb = np.asarray(stat.mean_conf[0])
+                        curr_mclb = np.asarray(stat.mean_conf[0][0])
                         min_static_mclbs[name] = np.min(np.append(
                             curr_mclb, min_static_mclbs[name]
                         ))
                     else:
-                        curr_mcub = np.asarray(stat.mean_conf[1])
+                        curr_mcub = np.asarray(stat.mean_conf[1][0])
                         max_nonstatic_mcubs[name] = np.max(np.append(
                             curr_mcub, max_nonstatic_mcubs[name]
                         ))
-                if stat.mean.ndim == 0:
+                if "v" in name:
+                    vel_mean = float(stat.mean[0])
+                    vel_mean_conf0 = float(stat.mean_conf[0][0])
+                    vel_mean_conf1 = float(stat.mean_conf[1][0])
+                    vel_std = float(stat.std_dev[0])
                     ps = "      {}: m={} ({}, {}), s={}".format(
-                        name, stat.mean, stat.mean_conf[0], stat.mean_conf[1],
-                        stat.std_dev
+                        name, vel_mean, vel_mean_conf0, vel_mean_conf1, vel_std
                     )
                     print(ps)
                 else:
