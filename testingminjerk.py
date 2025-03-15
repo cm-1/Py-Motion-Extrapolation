34d100302aca52c72a8584ecda3248b43e015010
Christopher Mossman <christopher.mossman@ucalgary.ca>
2025-03-14 20:19:24 -0600

    Testing min jerk with higher dimension and noise.

--- testingminjerk.py
+++ testingminjerk.py
@@ -7,40 +7,15 @@ import matplotlib.pyplot as plt
 import posemath as pm
 import minjerk as mj
 
-x_0 = np.array([1.35])
-x_f = np.array([7.5])
+x_0 = np.array([1.35, 3.35])
+x_f = np.array([7.5, 8.5])
+dim = len(x_0)
 
 t_f = 37.5
 
 ts = np.arange(t_f).reshape(-1, 1)
 ys = mj.min_jerk(t_f, ts, x_0, x_f)
-y_subintervals = [ys]
 
-last_t = ts[-1][0]
-last_y = x_f
-switch_ind = 2
-switch_time = 17
-for i in range(5):
-    wait_time = random.randint(1, 33)
-    if i == (switch_ind + 1):
-        wait_time = 0
-    y_subintervals.append(np.repeat(last_y, wait_time).reshape(-1, 1))
-    last_t += wait_time
-    next_x_f = last_y + 45 * (2.0 * random.random() - 1.0)
-    min_min_jerk_time = 33 if i == switch_ind else 4
-    jerk_time = random.randint(min_min_jerk_time, 35)
-    jerk_ts = np.arange(jerk_time).reshape(-1, 1)
-    if i == switch_ind:
-        jerk_ts = jerk_ts[:switch_time]
-    next_ys = mj.min_jerk(jerk_time, jerk_ts, last_y, next_x_f)
-    y_subintervals.append(next_ys)
-    last_y = next_ys[-1] if i == switch_ind else next_x_f
-    t_inc = jerk_time if i != switch_ind else switch_time
-    last_t += t_inc
-    print("wait time, jerk time, next x_f", wait_time, jerk_time, next_x_f)
-all_ts = np.arange(last_t + 1)
-
-y_concat = np.concatenate(y_subintervals, axis=0)
 
 
 #%%
@@ -67,25 +42,65 @@ print("max first preds diff:", np.max(np.abs(pred_diff)))
 # plt.plot(ts[-len(non_nan_first_preds):], non_nan_first_preds)
 # plt.show()
 #%%
-prev_translations = y_concat[:-1]
-translation_diffs = np.diff(y_concat, 1, axis=0)
+def scenario(max_noise = 0.0):
+    y_subintervals = [ys]
 
-deg1_vels = translation_diffs[:-1]
+    last_t = ts[-1][0]
+    last_y = x_f
+    switch_ind = 2
+    switch_time = 17
+    for i in range(5):
+        wait_time = random.randint(1, 33)
+        if i == (switch_ind + 1):
+            wait_time = 0
+        y_subintervals.append(np.repeat([last_y], wait_time, axis=0))
+        last_t += wait_time
+        next_x_f = last_y + 45 * (2.0 * np.random.random(dim) - 1.0)
+        min_min_jerk_time = 33 if i == switch_ind else 4
+        jerk_time = random.randint(min_min_jerk_time, 35)
+        jerk_ts = np.arange(jerk_time).reshape(-1, 1)
+        if i == switch_ind:
+            jerk_ts = jerk_ts[:switch_time]
+        next_ys = mj.min_jerk(jerk_time, jerk_ts, last_y, next_x_f)
+        rand_vals = np.random.random(next_ys.shape)
+        noise = 1.0 - (rand_vals + rand_vals)
+        next_ys += max_noise * noise
+        y_subintervals.append(next_ys)
+        last_y = next_ys[-1] if i == switch_ind else next_x_f
+        t_inc = jerk_time if i != switch_ind else switch_time
+        last_t += t_inc
+        print("wait time, jerk time, next x_f", wait_time, jerk_time, next_x_f)
+    all_ts = np.arange(last_t + 1)
 
-deg1_speeds_full = np.linalg.norm(deg1_vels, axis=-1, keepdims=True)
-unit_vels_deg1 = pm.safelyNormalizeArray(deg1_vels, deg1_speeds_full)
+    y_concat = np.concatenate(y_subintervals, axis=0)
+    
 
-t_diff_angs = pm.anglesBetweenVecs(
-    unit_vels_deg1[:-1], unit_vels_deg1[1:], False
-)
-d_under_thresh = deg1_speeds_full < 0.00001
-a_over_thresh = t_diff_angs > np.pi / 2
+    prev_translations = y_concat[:-1]
+    translation_diffs = np.diff(y_concat, 1, axis=0)
+
+    deg1_vels = translation_diffs[:-1]
+
+    deg1_speeds_full = np.linalg.norm(deg1_vels, axis=-1, keepdims=True)
+    unit_vels_deg1 = pm.safelyNormalizeArray(deg1_vels, deg1_speeds_full)
+
+    t_diff_angs = pm.anglesBetweenVecs(
+        unit_vels_deg1[:-1], unit_vels_deg1[1:], False
+    )
+    d_under_thresh = deg1_speeds_full < 0.00001
+    a_over_thresh = t_diff_angs > np.pi / 2
 
 
-preds = mj.min_jerk_lsq(
-    prev_translations, d_under_thresh.flatten(), a_over_thresh.flatten()
-)#, forget_fac: float = 0.91):
+    preds = mj.min_jerk_lsq(
+        prev_translations, d_under_thresh.flatten(), a_over_thresh.flatten()
+    )#, forget_fac: float = 0.91):
+
+    return all_ts, y_concat, preds
+
+
+
 #%%
-plt.plot(all_ts, y_concat)
-plt.plot(all_ts[-len(preds):], preds)
+all_ts, y_concat, preds = scenario(0.0001)
+draw_ind = 1
+plt.plot(all_ts, y_concat[:, draw_ind])
+plt.plot(all_ts[-len(preds):], preds[:, draw_ind])
 plt.show()
