bffa5fb366e4f0ebb1d966b7208b34f57cff8e6c
Christopher Mossman <christopher.mossman@ucalgary.ca>
2025-06-12 14:21:24 -0600

    Added crackle multpiliers

--- decision_tree_switch.py
+++ decision_tree_switch.py
@@ -346,33 +346,37 @@ def poseLossJAV(y_true, y_pred):
     # y_pred2 = y_pred + y_true[:, 9:15]
 
     pred_disp_0 = y_true[:, 0] * y_pred[:, 0] + y_true[:, 1] * y_pred[:, 1] \
-        + y_true[:, 3] * y_pred[:, 3] + y_true[:, 6] * y_pred[:, 6]
+        + y_true[:, 3] * y_pred[:, 3] + y_true[:, 6] * y_pred[:, 6] \
+        + y_true[:, 9] * y_pred[:, 9]
     pred_disp_1 = y_true[:, 2] * y_pred[:, 2] + y_true[:, 4] * y_pred[:, 4] \
-        + y_true[:, 7] * y_pred[:, 7]
-    pred_disp_2 = y_true[:, 5] * y_pred[:, 5] + y_true[:, 8] * y_pred[:, 8]
+        + y_true[:, 7] * y_pred[:, 7] + y_true[:, 10] * y_pred[:, 10]
+    pred_disp_2 = y_true[:, 5] * y_pred[:, 5] + y_true[:, 8] * y_pred[:, 8] \
+        + y_true[:, 11] * y_pred[:, 11]
 
     pred_disp = tf.stack([pred_disp_0, pred_disp_1, pred_disp_2], axis=-1)
     # pred_disp = tf.gather(y_true, (0,2,5), axis=-1) * y_pred
 
-    true_disp = y_true[:, 9:12] #6:9]
+    true_disp = y_true[:, 12:15] #6:9]
 
     err_vec3 = true_disp - pred_disp
     return tf.norm(err_vec3, axis=-1)
 
 def poseLossResidualJAV(y_true, y_pred):
 
-    y_pred2 = y_pred + y_true[:, 12:21] #9:15]
+    y_pred2 = y_pred + y_true[:, 15:27] #9:15]
 
     pred_disp_0 = y_true[:, 0] * y_pred2[:, 0] + y_true[:, 1] * y_pred2[:, 1] \
-        + y_true[:, 3] * y_pred2[:, 3] + y_true[:, 6] * y_pred2[:, 6]
+        + y_true[:, 3] * y_pred2[:, 3] + y_true[:, 6] * y_pred2[:, 6] \
+        + y_true[:, 9] * y_pred2[:, 9]
     pred_disp_1 = y_true[:, 2] * y_pred2[:, 2] + y_true[:, 4] * y_pred2[:, 4] \
-        + y_true[:, 7] * y_pred2[:, 7]
-    pred_disp_2 = y_true[:, 5] * y_pred2[:, 5] + y_true[:, 8] * y_pred2[:, 8]
+        + y_true[:, 7] * y_pred2[:, 7] + y_true[:, 10] * y_pred2[:, 10]
+    pred_disp_2 = y_true[:, 5] * y_pred2[:, 5] + y_true[:, 8] * y_pred2[:, 8] \
+        + y_true[:, 11] * y_pred2[:, 11]
 
     pred_disp = tf.stack([pred_disp_0, pred_disp_1, pred_disp_2], axis=-1)
     # pred_disp = tf.gather(y_true, (0,2,5), axis=-1) * y_pred
 
-    true_disp = y_true[:, 9:12] #6:9]
+    true_disp = y_true[:, 12:15] #6:9]
 
     err_vec3 = true_disp - pred_disp
     return tf.norm(err_vec3, axis=-1)
@@ -383,10 +387,12 @@ def getVelFrameDisplacements(y_true, y_pred):
     # y_pred2 = y_pred + y_true[:, 9:15]
     # Calculating the local displacement is the same as custom tf loss function.
     disp[:, 0] = y_true[:, 0] * y_pred[:, 0] + y_true[:, 1] * y_pred[:, 1] \
-        + y_true[:, 3] * y_pred[:, 3] + y_true[:, 6] * y_pred[:, 6]
+        + y_true[:, 3] * y_pred[:, 3] + y_true[:, 6] * y_pred[:, 6] \
+        + y_true[:, 9] * y_pred[:, 9]
     disp[:, 1] = y_true[:, 2] * y_pred[:, 2] + y_true[:, 4] * y_pred[:, 4] \
-        + y_true[:, 7] * y_pred[:, 7]
-    disp[:, 2] = y_true[:, 5] * y_pred[:, 5] + y_true[:, 8] * y_pred[:, 8]
+        + y_true[:, 7] * y_pred[:, 7] + y_true[:, 10] * y_pred[:, 10]
+    disp[:, 2] = y_true[:, 5] * y_pred[:, 5] + y_true[:, 8] * y_pred[:, 8] \
+        + y_true[:, 11] * y_pred[:, 11]
 
     return disp
 
@@ -478,7 +484,7 @@ def getUntrainedNN(loss = None, use_resid_data: bool = False):
         keras.layers.Dense(nodes_per_layer, activation=vel_nn_activation),
         keras.layers.Dropout(dropout_rate),
         keras.layers.Dense(nodes_per_layer, activation=vel_nn_activation),
-        keras.layers.Dense(9) #6)
+        keras.layers.Dense(12) #6)
     ])
     if loss is None:
         loss = poseLossJAV
@@ -1023,7 +1029,7 @@ def gtMultipliersJAV(y_true, bounds = None, tol: float = 0.001, max_iter: int = 
 
     if bounds is None:
         inv_mats = np.linalg.inv(muls_to_pt_mats)
-        return pm.einsumMatVecMul(inv_mats, y_true[:, 9:12]) #6:9])
+        return pm.einsumMatVecMul(inv_mats, y_true[:, 12:15]) #6:9])
     
     n = len(y_true)
     res = np.empty((n, 3))
@@ -1036,7 +1042,7 @@ def gtMultipliersJAV(y_true, bounds = None, tol: float = 0.001, max_iter: int = 
 
     for i in range(n):
         lsq_res = lsq_linear(
-            muls_to_pt_mats[i], y_true[i, 9:12], #6:9],
+            muls_to_pt_mats[i], y_true[i, 12:15], #6:9],
             bounds, tol=tol, max_iter=max_iter
         )
         res[i] = lsq_res.x
--- motiontools/posefeatures.py
+++ motiontools/posefeatures.py
@@ -1150,7 +1150,9 @@ def dataForCombosJAV(pose_loaders: PoseLoaderList, vec_order: OrderForJAV,
             accs = np.diff(vels[:-1], axis=0)
             jerks = np.diff(accs, axis=0)
             snaps = np.diff(jerks, axis=0)
+            crackles = np.diff(snaps, axis=0)
             snaps = np.insert(snaps, 0, np.zeros(3), axis=0)
+            crackles = np.concatenate((np.zeros((2, 3)), crackles), axis=0)
 
             # Here we specify which order in which we orthonormalize our
             # velocity, acceleration, and jerk vectors into orthonormal frames.
@@ -1172,6 +1174,7 @@ def dataForCombosJAV(pose_loaders: PoseLoaderList, vec_order: OrderForJAV,
             # local_vecs2 = pm.einsumMatVecMul(mats, ordered[2])
             local_diffs = pm.einsumMatVecMul(mats, vels[3:])
             local_snaps = pm.einsumMatVecMul(mats, snaps)
+            local_crackles = pm.einsumMatVecMul(mats, crackles)
 
             # We'll now return all of the data needed to convert velocity,
             # acceleration, and jerk multipliers into local vectors in these
@@ -1180,7 +1183,10 @@ def dataForCombosJAV(pose_loaders: PoseLoaderList, vec_order: OrderForJAV,
             # frame (a vector [speed, 0, 0]), the acceleration in this frame
             # (i.e. [a_p, a_o, 0]), etc. And since we don't need to return 0s,
             # we can just return the following:
-            c_res = (*all_mags, *(local_snaps.T), *(local_diffs.T))
+            c_res = (
+                *all_mags, *(local_snaps.T), *(local_crackles.T),
+                *(local_diffs.T)
+            )
 
             all_data[skip][c] = np.stack(c_res, axis=-1)
             if return_world2locals:
