b1ae514b4a3c9e17aeb186560d70f949153a262f
Christopher Mossman <christopher.mossman@ucalgary.ca>
2025-08-27 09:51:25 -0600

    NN extrapolation plots for real data.

--- hypothetical_point_vis.py
+++ hypothetical_point_vis.py
@@ -8,9 +8,11 @@ from numpy.typing import NDArray
 import keras
 
 from motiontools.posefeatures import (
-    CalcsForVideo, dataForPositionsJAV, JAV, getWorldFrameDisplacements
+    CalcsForVideo, dataForPositionsJAV, JAV, getWorldFrameDisplacements,
+    PositionDerivativeCollection
 )
 from motiontools.dataorg import UnitAwareScaler
+import posemath as pm
 
 # Needed when loading model, even though IDE syntax highlighting marks this as
 # "unused"!
@@ -24,7 +26,7 @@ with open("./results/models/scaler.pickle", "rb") as f:
     scaler = pickle.load(f)
 selected_keys = scaler.column_keys
 
-matching_models = glob.glob("./results/models/JAV_MULTIPLIERS*.keras")
+matching_models = glob.glob("./results/models/hot_1024*.keras")
 chosen_model = sorted(matching_models)[-1]
 print("Loading model:", chosen_model)
 loaded_model = keras.models.load_model(chosen_model, custom_objects={"poseLossJAV": poseLossJAV})
@@ -60,18 +62,48 @@ def processAllInputs(tupleList: typing.Tuple[NDArray, NDArray, NDArray]):
 # assumes the last one is ground truth for which it shouldn't generate any
 # predictions, and we need 6 input points to calculate nonzero crackle.
 rand_pts = np.random.normal(0.0, 0.0010, (7,3))# zeros((7,3))
-default_aas = np.ones_like(rand_pts)
 rand_pts[:-4] = 0
 origin_pt = rand_pts[-4]
 origin_pt[:] = 0
 last_fixed_pt = rand_pts[-3]
 last_fixed_pt[:] = (15, 0, 0)
-print(end="")
+
+default_aas = np.ones_like(rand_pts)
+
+
+from gtCommon import PoseLoaderBCOT
+all_bcot_ids = PoseLoaderBCOT.getAllIDs(True)
+train_bcot_ids, val_bcot_ids, test_bcot_ids = \
+    PoseLoaderBCOT.trainValidationTestByBody(0.1, 0.2, 0)
+test_loaders = [PoseLoaderBCOT(*v[:2]) for v in test_bcot_ids]
+found_ind = -1
+for tl in test_loaders:
+    tl_diffs = np.diff(tl.getTranslationsGTNP(), 1, axis=0)
+    tl_diff_subset = tl_diffs[:6]
+    for i in range(0, len(tl_diffs) - 6 + 1):
+        tl_diff_subset = tl_diffs[i:(i+6)]
+        if np.sum(np.linalg.norm(tl_diff_subset[:4], axis=-1)) < 4.0:
+            found_ind = i
+            # break
+    if found_ind >= 0:
+        tl_diff_subset = tl_diffs[found_ind:(found_ind + 6)]
+        default_aas = tl.getRotationsGTNP()[found_ind:(found_ind+7)]
+        diff_mat = pm.getOrthonormalFrames(
+            True, tl_diff_subset[-3:-2], tl_diff_subset[-2:-1]
+        )[1][0]
+        rand_pts = tl.getTranslationsGTNP()[found_ind:found_ind + 7]
+        new_frame_pts = (rand_pts @ diff_mat.T)
+        rand_pts = new_frame_pts - new_frame_pts[-3]
+        break
+last_fixed_pt = rand_pts[-3]
+#%%
+
 outlist = []
 all_in_disp_mags = (np.arange(GRAPH_RES) / GRAPH_RES) * 2 - 1
-all_in_disp_mags *= DISP_RADIUS
+all_in_disp_mags *= max(DISP_RADIUS, np.linalg.norm(rand_pts[-3] - rand_pts[-2]) + 5)
 x_pts = last_fixed_pt[0] + all_in_disp_mags
 y_pts = last_fixed_pt[1] + all_in_disp_mags
+print(end="")
 for xi, x in enumerate(x_pts):
     print("\rx index =", xi, end="", flush=True)
     for yi, y in enumerate(y_pts):
@@ -114,19 +146,20 @@ fig = go.Figure()
 
 # Add the three xyz surfaces
 
-# for level, name in enumerate("xyz"):
-#     fig.add_trace(go.Surface(
-#         x=X, y=Y, z=rand_out_vec3s[..., level],
-#         opacity=0.32, name=name, showscale=False
-#     ))
+for level, name in enumerate("xyz"):
+    fig.add_trace(go.Surface(
+        x=X, y=Y, z=rand_out_vec3s[..., level],
+        opacity=0.32, name=name, showscale=False
+    ))
 
 
 disp_mags = np.linalg.norm(rand_out_vec3s - xyz_ins, axis=-1)
 # Add |d| surface
-# fig.add_trace(go.Surface(
-#     x=X, y=Y, z=disp_mags,
-#     opacity=0.32, name="|d|", showscale=False, legendrank=2
-# ))
+fig.add_trace(go.Surface(
+    x=X, y=Y, z=disp_mags,
+    opacity=0.32, name="|d|", showscale=False, legendrank=2
+))
+
 
 disp_mags2 = np.linalg.norm(xyz_ins - last_fixed_pt, axis=-1).flatten()
 out_vec3s_list = rand_out_vec3s.reshape(-1, 3)
@@ -151,7 +184,7 @@ fig.add_trace(go.Scatter3d(
         colorscale='Viridis',   # choose a colorscale
         opacity=0.8
     )
-))
+)) 
 
 # Add polyline and markers
 fig.add_trace(go.Scatter3d(
@@ -166,7 +199,7 @@ fig.add_trace(go.Scatter3d(
 fig.update_layout(
     scene=dict(
         xaxis_title="x", yaxis_title="y", zaxis_title="z",
-        aspectmode="cube"
+        aspectmode="data"
     )
 )
 fig.update_traces(showlegend=True)#, showscale=False)
