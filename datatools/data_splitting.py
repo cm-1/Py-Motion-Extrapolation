d68eb62066917f1cea46cd2ba788cf282176041b
Christopher Mossman <christopher.mossman@ucalgary.ca>
2025-11-11 18:01:52 -0700

    DataSubsetKind fromName(name: str) function added.

--- datatools/data_splitting.py
+++ datatools/data_splitting.py
@@ -11,3 +11,11 @@ class DataSubsetKind(Enum):
         return (
             DataSubsetKind.TRAIN, DataSubsetKind.TEST, DataSubsetKind.VALIDATION
         )
+
+    @staticmethod
+    def fromName(name: str):
+        upper_name = name.upper()
+        for _dsk in DataSubsetKind:
+            if _dsk.name == upper_name:
+                return _dsk
+        raise ValueError(upper_name + " is not a valid DataSubsetKind!")
