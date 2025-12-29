a7368273d67f673a59ca6d0634663fd54c253d2a
Christopher Mossman <christopher.mossman@ucalgary.ca>
2025-12-28 22:32:13 -0700

    zeros_like issues in tflite export

--- nn_utilities/nn_export.py
+++ nn_utilities/nn_export.py
@@ -33,42 +33,50 @@ class ModelExportWrapper(tf.Module):
             tape.watch(x)
             y = self.model(x, training=False)
             y_unstacked = tf.unstack(y, axis=1)
-        grads = []
         
-        for y_slice in y_unstacked:
-            # Now we take the gradient of the unstacked slice directly
-            grad = tape.gradient(y_slice, x)
-            grads.append(grad)
+        def temp_grad(y_ind):
+            y_slice = y_unstacked[y_ind]
+            return tape.gradient(y_slice, x)
 
-        jacobian = tf.stack(grads, axis=1)
+        jacobian = tf.stack([
+            temp_grad(0), temp_grad(1), temp_grad(2), temp_grad(3),
+            temp_grad(4), temp_grad(5)
+        ], axis=1)
         return jacobian
     
+    # ff2 = wrapper.get_frozen_func(wrapper.jacobian_split)
+    # ops_all2 = [o.name for o in ff2.graph.operations]
+    # len([o for o in ops_all2 if "zeros_like" in o.lower()])
+    '''
+    ERROR: C:/users/username/_bazel_username/sueumax6/external/snappy/BUILD.bazel:89:8: Executing genrule @snappy//:snappy_stubs_public_h failed: (Exit 1): bash.exe failed: error executing command (from target @snappy//:snappy_stubs_public_h)
+    cd /d C:/users/username/_bazel_username/sueumax6/execroot/org_tensorflow
+    SET CLANG_COMPILER_PATH=C:Program FilesLLVMbinclang.exe
+        SET PATH=C:\Windows\system32;C:\WINDOWS;C:\WINDOWS\System32;C:\WINDOWS\System32\WindowsPowerShell\v1.0;C:\Users\username\AppData\Local\bazelisk\downloads\sha256\6eae8e7f28e1b68b833503d1a58caf139c11e52de19df0d787d974653a0ea4c6\bin;C:\Users\username\Documents\python_venvs\building_tf\Scripts;C:\Python314\Scripts\;C:\Python314\;C:\Python313\Scripts\;C:\Python313\;C:\Python312\Scripts\;C:\Python312\;C:\Python311\Scripts\;C:\Python311\;C:\VulkanSDK\1.3.204.1\Bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\bin;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\libnvvp;C:\Python310\Scripts\;C:\Python310\;C:\Python39\Scripts\;C:\Python39\;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\include;C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v10.1\extras\CUPTI\lib64;C:\tools\cuda\bin;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Windows\System32\OpenSSH\;C:\ProgramData\chocolatey\bin;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\Microsoft VS Code\bin;C:\Program Files\NVIDIA Corporation\Nsight Compute 2019.4.0\;C\tools\cuda\lib\x64;C:\Program Files (x86)\gnupg\bin;C:\Qt\5.15.1\msvc2019_64\bin;C:\Libraries\opencv-481\x64\vc16\bin;C:\Libraries\assimp-520\bin;C:\tools\BCURRAN3;C:\Program Files\Java\jdk1.8.0_211\bin;C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Tools\MSVC\14.29.30037\bin\Hostx64\x64;C:\Program Files (x86)\NVIDIA Corporation\PhysX\Common;C:\Program Files\Meld\;C:\WINDOWS\system32;C:\WINDOWS;C:\WINDOWS\System32\Wbem;C:\WINDOWS\System32\WindowsPowerShell\v1.0\;C:\WINDOWS\System32\OpenSSH\;C:\Program Files\CMake\bin;C:\Program Files\Git\cmd;C:\Users\username\scoop\shims;C:\Users\username\AppData\Local\Microsoft\WindowsApps;C:\Users\username\AppData\Local\Microsoft\WindowsApps;C:\PortablePrograms\glew-2.2.0-win32\glew-2.2.0\bin\Release\x64;C:\Users\username\AppData\Local\Programs\Ollama;C:\Users\username\AppData\Local\Microsoft\WinGet\Packages\ggml.llamacpp_Microsoft.Winget.Source_8wekyb3d8bbwe;C:\Users\username\AppData\Local\PowerToys\DSCModules\;C:\Users\username\AppData\Local\Microsoft\WinGet\Packages\Bazel.Bazelisk_Microsoft.Winget.Source_8wekyb3d8bbwe;;C:\msys64\usr\bin
+        SET PYTHON_BIN_PATH=C:/Users/username/Documents/python_venvs/building_tf/Scripts/python.exe
+        SET PYTHON_LIB_PATH=C:/Users/username/Documents/python_venvs/building_tf/lib/site-packages
+        SET TF2_BEHAVIOR=1
+    C:\Windows\system32\bash.exe -c source external/bazel_tools/tools/genrule/genrule-setup.sh; sed -e 's/${\(.*\)_01}/\1/g' -e 's/${SNAPPY_MAJOR}/1/g' -e 's/${SNAPPY_MINOR}/1/g' -e 's/${SNAPPY_PATCHLEVEL}/4/g' external/snappy/snappy-stubs-public.h.in >bazel-out/x64_windows-opt/bin/external/snappy/snappy-stubs-public.h
+    # Configuration: 89aeafe5f56cecccb8c7d42150516b77f11f26ebfd703d19f64b0d3a70eb37b9
+    # Execution platform: @local_execution_config_platform//:platform
+    /bin/bash: source external/bazel_tools/tools/genrule/genrule-setup.sh; sed -e 's/${\(.*\)_01}/\1/g' -e 's/${SNAPPY_MAJOR}/1/g' -e 's/${SNAPPY_MINOR}/1/g' -e 's/${SNAPPY_PATCHLEVEL}/4/g' external/snappy/snappy-stubs-public.h.in >bazel-out/x64_windows-opt/bin/external/snappy/snappy-stubs-public.h: bad substitution
+    Target //tensorflow/lite/delegates/flex:tensorflowlite_flex failed to build
+    INFO: Elapsed time: 270.593s, Critical Path: 5.38s
+    INFO: 115 processes: 101 internal, 14 local.
+    FAILED: Build did NOT complete successfully
+    '''
     def jacobian(self, x):
         x.set_shape([1, 36])
         jacobian_cols = []
-         # 2. Iterate over the INPUT dimension (36)
-        # We compute one column of the Jacobian at a time.
         for i in range(36):
-            # Create a "tangent" vector (basis vector)
-            # This represents a perturbation of 1.0 at input index i
-            # We use a constant one-hot vector to keep the graph static.
-            tangent = tf.one_hot(indices=i, depth=36, on_value=1, off_value=0, dtype=tf.int64)
+            tangent = tf.one_hot(indices=i, depth=36, on_value=1, off_value=0, dtype=tf.float32)
             tangent = tf.reshape(tangent, (1, 36))
             
-            # 3. Forward Accumulator
-            # This traces the graph pushing the tangent forward.
-            # No backward pass is generated!
             with tf.autodiff.ForwardAccumulator(x, tangent) as acc:
                 y = self.model(x, training=False)
                 
-            # 4. Get the Jacobian-Vector Product (JVP)
-            # This is the gradient of y w.r.t input[i]
             col = acc.jvp(y)
             jacobian_cols.append(col)
             
-        # 5. Stack columns to form the full Jacobian matrix
-        # col shape is (1, OutputDim)
-        # Stack shape will be (1, OutputDim, 36)
         jacobian = tf.stack(jacobian_cols, axis=2)
         
         return jacobian
